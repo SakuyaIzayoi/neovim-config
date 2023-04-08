78ef7177854161f4f0db053476c2c3276783e3cf
Alessandro Gangemi <agangemi0x2a@gmail.com>
2023-04-08 10:23:57 -0400

    Fix issue with lua_ls

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -2,7 +2,7 @@ local lsp = require('lsp-zero')
 lsp.preset('recommended')
 lsp.nvim_workspace()
 
-lsp.configure('sumneko_lua', {
+lsp.configure('lua_ls', {
     settings = {
         Lua = {
             diagnostics = {
