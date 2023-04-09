0271df60d775b8cd7e7f3f78266ddae8dbfbe360
Alessandro Gangemi <agangemi0x2a@gmail.com>
2023-04-08 23:28:52 -0400

    Clean up nvim-cmp

--- after/plugin/luasnip.lua
+++ after/plugin/luasnip.lua
@@ -1,16 +1,13 @@
-local has_words_before = function()
-  unpack = unpack or table.unpack
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-end
-
-local luasnip = require("luasnip")
 local cmp = require("cmp")
+local cmp_action = require('lsp-zero').cmp_action()
+
+require('luasnip.loaders.from_vscode').lazy_load()
 
 cmp.setup({
-
-  -- ... Your other configuration ...
-
+  preselect = 'item',
+  completion = {
+      completeopt = 'menu,menuone,noinsert'
+  },
   sources = {
       {name = 'path'},
       {name = 'nvim_lsp'},
@@ -19,35 +16,8 @@ cmp.setup({
       {name = 'luasnip', keyword_length = 2},
   },
   mapping = {
-
-    -- ... Your other mappings ...
-
-    ["<Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable() 
-      -- they way you will only jump inside the snippet region
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
-      elseif has_words_before() then
-        cmp.complete()
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
-
-    ["<S-Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
-
-    -- ... Your other mappings ...
+    ['<CR>'] = cmp.mapping.confirm({select = false}),
+    ["<Tab>"] = cmp_action.luasnip_supertab(),
+    ["<S-Tab>"] = cmp_action.luasnip_shift_supertab(),
   },
-
-  -- ... Your other configuration ...
 })
