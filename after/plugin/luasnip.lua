ce46647dcd17d36c43329ec3ce6289d3aef7c03e
Alessandro Gangemi <agangemi0x2a@gmail.com>
2023-04-08 23:18:58 -0400

    Add lua source for nvim-cmp

--- after/plugin/luasnip.lua
+++ after/plugin/luasnip.lua
@@ -14,6 +14,7 @@ cmp.setup({
   sources = {
       {name = 'path'},
       {name = 'nvim_lsp'},
+      {name = 'nvim_lua'},
       {name = 'buffer', keyword_length = 3},
       {name = 'luasnip', keyword_length = 2},
   },
