bb201477bed4c24059ef6f344e2393e3dc087315
Alessandro Gangemi <agangemi0x2a@gmail.com>
2023-05-01 09:32:13 -0400

    Add rust-tools and clippy

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -12,23 +12,33 @@ lsp.configure('lua_ls', {
     }
 })
 
+-- rust-tools uses nvim-lspconfig under the hood so skip lsp-zero's config
+lsp.skip_server_setup({ 'rust-analyzer' })
+local rt = require('rust-tools')
+rt.setup({
+    server = {
+        on_attach = function(_, bufnr)
+            vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
+        end,
+        settings = {
+            ["rust-analyzer"] = {
+                checkOnSave = {
+                    command = "clippy"
+                },
+            },
+        },
+    },
+})
+
 lsp.setup()
+
+-- Treesitter folding
+vim.wo.foldmethod = 'expr'
+vim.wo.foldexpr = 'nvim_treesitter#foldexpr()'
+
+-- Trouble
+vim.keymap.set("n", "<leader>xx", "<cmd>TroubleToggle<cr>", { silent = true, noremap = true })
 
 vim.diagnostic.config({
     virtual_text = true,
-})
-
-require("lsp-inlayhints").setup()
-vim.api.nvim_create_augroup("LspAttach_inlayhints", {})
-vim.api.nvim_create_autocmd("LspAttach", {
-    group = "LspAttach_inlayhints",
-    callback = function(args)
-        if not (args.data and args.data.client_id) then
-            return
-        end
-
-        local bufnr = args.buf
-        local client = vim.lsp.get_client_by_id(args.data.client_id)
-        require("lsp-inlayhints").on_attach(client, bufnr)
-    end,
 })
--- lua/lichform/packer.lua
+++ lua/lichform/packer.lua
@@ -5,13 +5,13 @@ return require('packer').startup(function(use)
     --use 'christoomey/vim-tmux-navigator'
     use 'aserowy/tmux.nvim'
     use { 'nathangrigg/vim-beancount', ft = 'beancount' }
-    use { 'hashivim/vim-terraform', ft = {'terraform'} }
-    use { 'elixir-editors/vim-elixir', ft = {'elixir'} }
-    use { 'pearofducks/ansible-vim', ft = {'yaml'} }
-    use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
+    use { 'hashivim/vim-terraform', ft = { 'terraform' } }
+    use { 'elixir-editors/vim-elixir', ft = { 'elixir' } }
+    use { 'pearofducks/ansible-vim', ft = { 'yaml' } }
+    use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
     use 'kyazdani42/nvim-web-devicons'
     use 'kyazdani42/nvim-tree.lua'
-    use { 'akinsho/bufferline.nvim', tag = 'v2.*', requires = 'kyazdani42/nvim-web-devicons'}
+    use { 'akinsho/bufferline.nvim', tag = 'v2.*', requires = 'kyazdani42/nvim-web-devicons' }
     use { 'kylechui/nvim-surround',
         tag = '*',
         config = function()
@@ -20,13 +20,14 @@ return require('packer').startup(function(use)
     }
     use 'jiangmiao/auto-pairs'
     use 'sainnhe/gruvbox-material'
-    use { 'dstein64/vim-startuptime', cmd = {'StartupTime'} }
+    use { 'dstein64/vim-startuptime', cmd = { 'StartupTime' } }
     use {
         'nvim-telescope/telescope.nvim', tag = '0.1.0',
-        requires = {{'nvim-lua/plenary.nvim'}}}
+        requires = { { 'nvim-lua/plenary.nvim' } } }
     use {
         'nvim-telescope/telescope-fzf-native.nvim',
-        run = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' }
+        run =
+        'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' }
     use {
         'nvim-lualine/lualine.nvim',
         requires = { 'kyazdani42/nvim-web-devicons', opt = true }
@@ -41,34 +42,39 @@ return require('packer').startup(function(use)
         'VonHeikemen/lsp-zero.nvim',
         requires = {
             -- Basic LSP Support
-            {'neovim/nvim-lspconfig'},
+            { 'neovim/nvim-lspconfig' },
             {
                 'williamboman/mason.nvim',
-                run = function ()
+                run = function()
                     pcall(vim.cmd, 'MasonUpdate')
                 end,
             },
-            {'williamboman/mason-lspconfig.nvim'},
+            { 'williamboman/mason-lspconfig.nvim' },
 
             -- Autocomplete
-            {'hrsh7th/nvim-cmp'},
-            {'hrsh7th/cmp-buffer'},
-            {'hrsh7th/cmp-path'},
-            {'saadparwaiz1/cmp_luasnip'},
-            {'hrsh7th/cmp-nvim-lsp'},
-            {'hrsh7th/cmp-nvim-lua'},
+            { 'hrsh7th/nvim-cmp' },
+            { 'hrsh7th/cmp-buffer' },
+            { 'hrsh7th/cmp-path' },
+            { 'saadparwaiz1/cmp_luasnip' },
+            { 'hrsh7th/cmp-nvim-lsp' },
+            { 'hrsh7th/cmp-nvim-lua' },
 
             -- Snippets
-            {'L3MON4D3/LuaSnip'},
-            {'rafamadriz/friendly-snippets'},
+            { 'L3MON4D3/LuaSnip' },
+            { 'rafamadriz/friendly-snippets' },
         }
     }
-    use {'lvimuser/lsp-inlayhints.nvim'}
+    --use {'lvimuser/lsp-inlayhints.nvim'}
     use {
         'lewis6991/gitsigns.nvim',
-        config = function ()
+        config = function()
             require('gitsigns').setup()
         end
     }
+    use { 'simrat39/rust-tools.nvim' }
+    use { 'folke/trouble.nvim',
+        requires = 'nvim-tree/nvim-web-devicons',
+        config = function()
+            require("trouble").setup {}
+        end }
 end)
-
