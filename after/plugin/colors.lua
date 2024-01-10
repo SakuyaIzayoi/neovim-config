1b5afc64b3d022bc302ebac1c4643db9c06fbbda
Alessandro Gangemi <agangemi0x2a@gmail.com>
2024-01-10 14:48:11 -0500

    Remove some italics

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -5,6 +5,11 @@ vim.g.gruvbox_material_enable_bold = true
 --vim.g.gruvbox_material_transparent_background = true
 vim.g.gruvbox_material_diagnostic_text_highlight = true
 vim.g.gruvbox_material_diagnostic_line_highlight = true
+require('rose-pine').setup({
+	highlight_groups = {
+		['@variable'] = { italic = false },
+	},
+})
 vim.cmd('color rose-pine')
 
 require('transparent').setup {
