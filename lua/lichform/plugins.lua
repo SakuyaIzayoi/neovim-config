3a2c7d84cc1ba27762ef4b09cfbcc353336ac95e
Alessandro Gangemi <agangemi0x2a@gmail.com>
2024-03-05 00:27:16 -0500

    Move Telescope and Treesitter configs to plugins.lua

--- lua/lichform/plugins.lua
+++ lua/lichform/plugins.lua
@@ -26,6 +26,24 @@ require("lazy").setup({
     {
         'nvim-tree/nvim-web-devicons',
         lazy = true,
+        config = function ()
+            require('nvim-treesitter.configs').setup({
+                auto_install = true,
+                sync_install = false,
+                ignore_install = {},
+                highlight = {
+                    enable = true,
+                },
+                ensure_installed = {
+                    'c',
+                    'lua',
+                    'vim',
+                    'vimdoc',
+                    'query',
+                },
+                modules = {},
+            })
+        end
     },
     {
         'nvim-tree/nvim-tree.lua',
@@ -46,10 +64,31 @@ require("lazy").setup({
     'sainnhe/gruvbox-material',
     { 'rose-pine/neovim', name = 'rose-pine', priority = 1000 },
     { 'dstein64/vim-startuptime', cmd = "StartupTime" },
-    { 'nvim-telescope/telescope.nvim', tag = '0.1.5', dependencies = { 'nvim-lua/plenary.nvim' } },
+    {
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.5',
+        dependencies = { 'nvim-lua/plenary.nvim' },
+        config = function()
+            local builtin = require('telescope.builtin')
+            vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+            vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+            vim.keymap.set('n', '<leader>ps', function()
+                builtin.grep_string({ search = vim.fn.input("Grep > ") });
+            end)
+
+            vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+            vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+            vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+            vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+            vim.keymap.set('n', '<leader>gd', builtin.diagnostics, {})
+            vim.keymap.set('n', '<leader>gk', builtin.keymaps, {})
+            vim.keymap.set('n', '<leader>gr', builtin.registers, {})
+        end
+    },
     {
         'nvim-telescope/telescope-fzf-native.nvim',
-        build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
+        build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build',
     },
     {
         'nvim-lualine/lualine.nvim',
