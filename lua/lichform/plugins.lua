e596b4cd089c182f1d9ea0287d1b695899c82082
Alessandro Gangemi <agangemi0x2a@gmail.com>
2024-03-05 19:23:35 -0500

    Move nvim-ufo config to plugins.lua

--- lua/lichform/plugins.lua
+++ lua/lichform/plugins.lua
@@ -287,7 +287,23 @@ require("lazy").setup({
             { '<leader>xx', '<cmd>TroubleToggle<cr>', noremap = true },
         },
     },
-    { 'kevinhwang91/nvim-ufo', dependencies = 'kevinhwang91/promise-async' },
+    {
+        'kevinhwang91/nvim-ufo',
+        dependencies = 'kevinhwang91/promise-async',
+        config = function()
+            vim.o.foldcolumn = '0'
+            vim.o.foldlevel = 99
+            vim.o.foldlevelstart = 99
+            vim.o.foldenable = true
+
+            local ufo = require('ufo')
+
+            vim.keymap.set('n', 'zR', ufo.openAllFolds)
+            vim.keymap.set('n', 'zM', ufo.closeAllFolds)
+
+            ufo.setup()
+        end,
+    },
     {
         'startup-nvim/startup.nvim',
         dependencies = {
