539bd71e688c9f9fbfd05402d470aa485d1ed64b
Alessandro Gangemi <agangemi0x2a@gmail.com>
2024-03-07 18:12:24 -0500

    Move some Harpoon keybinds to lazy

--- lua/lichform/plugins/harpoon.lua
+++ lua/lichform/plugins/harpoon.lua
@@ -4,23 +4,18 @@ return {
         branch = 'harpoon2',
         dependencies = { 'nvim-lua/plenary.nvim', 'nvim-telescope/telescope.nvim' },
         keys = {
-            '<leader>a',
+            { '<leader>a', function() require('harpoon'):list():append() end,  desc = 'Harpoon Append' },
+            { '<M-1>',     function() require('harpoon'):list():select(1) end, desc = 'Harpoon File 1' },
+            { '<M-2>',     function() require('harpoon'):list():select(2) end, desc = 'Harpoon File 2' },
+            { '<M-3>',     function() require('harpoon'):list():select(3) end, desc = 'Harpoon File 3' },
+            { '<M-4>',     function() require('harpoon'):list():select(4) end, desc = 'Harpoon File 4' },
             '<C-e>',
-            '<M-1>',
-            '<M-2>',
-            '<M-3>',
-            '<M-4>',
         },
         config = function()
             local harpoon = require('harpoon')
             harpoon:setup({})
 
-            vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
             -- vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-            vim.keymap.set("n", "<M-1>", function() harpoon:list():select(1) end)
-            vim.keymap.set("n", "<M-2>", function() harpoon:list():select(2) end)
-            vim.keymap.set("n", "<M-3>", function() harpoon:list():select(3) end)
-            vim.keymap.set("n", "<M-4>", function() harpoon:list():select(4) end)
             local conf = require("telescope.config").values
             local function toggle_telescope(harpoon_files)
                 local file_paths = {}
