b5d8714ce72129f44cbff22f5dcd5922d629d5db
Alessandro Gangemi <agangemi0x2a@gmail.com>
2024-03-08 13:55:22 -0500

    Add nvim-treesitter-context

--- lua/lichform/plugins/treesitter.lua
+++ lua/lichform/plugins/treesitter.lua
@@ -1,26 +1,43 @@
 return {
-    'nvim-treesitter/nvim-treesitter',
-    build = ":TSUpdate",
-    event = { 'BufReadPost', 'BufNewFile', 'BufWritePre' },
-    main = 'nvim-treesitter.configs',
-    opts = {
-        auto_install = true,
-        highlight = {
-            enable = true,
-        },
-        indent = {
-            enable = true,
-        },
-        ensure_installed = {
-            'c',
-            'lua',
-            'vim',
-            'vimdoc',
-            'query',
+    {
+        'nvim-treesitter/nvim-treesitter',
+        build = ":TSUpdate",
+        event = { 'BufReadPost', 'BufNewFile', 'BufWritePre' },
+        main = 'nvim-treesitter.configs',
+        opts = {
+            auto_install = true,
+            highlight = {
+                enable = true,
+            },
+            indent = {
+                enable = true,
+            },
+            ensure_installed = {
+                'c',
+                'lua',
+                'vim',
+                'vimdoc',
+                'query',
+            },
         },
     },
     {
         'IndianBoy42/tree-sitter-just',
         ft = 'just',
+    },
+    {
+        'nvim-treesitter/nvim-treesitter-context',
+        event = { 'BufReadPost', 'BufNewFile', 'BufWritePre' },
+        opts = { mode = 'cursor', max_lines = 3 },
+        keys = {
+            {
+                '<leader>cc',
+                function()
+                    local tsc = require('treesitter-context')
+                    tsc.toggle()
+                end,
+                desc = "Toggle Treesitter Context",
+            },
+        },
     },
 }
