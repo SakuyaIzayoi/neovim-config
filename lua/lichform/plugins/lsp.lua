49e5227271fa8d545d0a8d8e96d3fb9ee513619f
Alessandro Gangemi <agangemi0x2a@gmail.com>
2024-03-08 13:20:21 -0500

    Adapt snip/completion loading from LazyVim

--- lua/lichform/plugins/lsp.lua
+++ lua/lichform/plugins/lsp.lua
@@ -18,28 +18,19 @@ return {
         'hrsh7th/nvim-cmp',
         event = "InsertEnter",
         dependencies = {
-            {
-                'L3MON4D3/LuaSnip',
-                dependencies = {
-                    'rafamadriz/friendly-snippets',
-                },
-            },
             'hrsh7th/cmp-buffer',
             'hrsh7th/cmp-path',
-            'saadparwaiz1/cmp_luasnip',
             'hrsh7th/cmp-nvim-lua',
             'hrsh7th/cmp-nvim-lsp',
         },
-        config = function()
+        opts = function()
             local lsp_zero = require('lsp-zero')
             lsp_zero.extend_cmp()
 
             local cmp = require('cmp')
             local cmp_action = require('lsp-zero').cmp_action()
 
-            require('luasnip.loaders.from_vscode').lazy_load()
-
-            cmp.setup({
+            return {
                 preselect = 'item',
                 completion = {
                     completeopt = 'menu,menuone,noinsert'
@@ -49,15 +40,58 @@ return {
                     { name = 'nvim_lsp' },
                     { name = 'nvim_lua' },
                     { name = 'buffer',  keyword_length = 3 },
-                    { name = 'luasnip', keyword_length = 2 },
                 },
                 mapping = {
                     ['<CR>'] = cmp.mapping.confirm({ select = false }),
                     ["<Tab>"] = cmp_action.luasnip_supertab(),
                     ["<S-Tab>"] = cmp_action.luasnip_shift_supertab(),
                 },
-            })
+            }
         end,
+        config = function(_, opts)
+            for _, source in ipairs(opts.sources) do
+                source.group_index = source.group_index or 1
+            end
+            require('cmp').setup(opts)
+        end,
+    },
+    {
+        'L3MON4D3/LuaSnip',
+        dependencies = {
+            {
+                'rafamadriz/friendly-snippets',
+                config = function()
+                    require('luasnip.loaders.from_vscode').lazy_load()
+                end,
+            },
+            {
+                'nvim-cmp',
+                dependencies = {
+                    'saadparwaiz1/cmp_luasnip',
+                },
+                opts = function(_, opts)
+                    opts.snippet = {
+                        expand = function(args)
+                            require('luasnip').lsp_expand(args.body)
+                        end,
+                    }
+                    table.insert(opts.sources, { name = 'luasnip' })
+                end,
+            },
+        },
+        keys = {
+            {
+                '<tab>',
+                function()
+                    return require('luasnip').jumpable(1) and '<Plug>luasnip-jump-next' or '<tab>'
+                end,
+                expr = true,
+                silent = true,
+                mode = 'i',
+            },
+            { '<tab>',   function() require('luasnip').jump(1) end,  mode = 's' },
+            { '<s-tab>', function() require('luasnip').jump(-1) end, mode = { 'i', 's' } },
+        },
     },
     {
         'williamboman/mason-lspconfig.nvim',
