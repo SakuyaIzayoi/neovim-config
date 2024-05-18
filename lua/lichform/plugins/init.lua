e5ec59557a6bd51cc6f17abb614ef1ee2eaf3141
Alessandro Gangemi <agangemi0x2a@gmail.com>
2024-05-18 16:52:30 -0400

    Lazy load Oil

--- lua/lichform/plugins/init.lua
+++ lua/lichform/plugins/init.lua
@@ -22,24 +22,29 @@ return {
 	{
 		"stevearc/oil.nvim",
 		dependencies = { "nvim-tree/nvim-web-devicons" },
-		config = function()
-			require("oil").setup({
-				columns = { "icon" },
-				keymaps = {
-					["<C-h>"] = false,
-					["M-h"] = "actions.select_split",
-				},
-				view_options = {
-					show_hidden = true,
-				},
-			})
-
-			-- Open parent directory in current window
-			vim.keymap.set("n", "-", "<cmd>Oil<cr>", { desc = "Open parent directory" })
-
-			-- Open parent directory in floating window
-			vim.keymap.set("n", "<space>-", require("oil").toggle_float)
-		end,
+		keys = {
+			{ "-", "<cmd>Oil<cr>", mode = "n", desc = "Oil: Open parent directory" },
+			{
+				"<space>-",
+				function()
+					require("oil").toggle_float()
+				end,
+				mode = "n",
+				desc = "Oil: Open parent dir in floating window",
+			},
+		},
+		cmd = "Oil",
+		opts = {
+			columns = { "icon" },
+			keymaps = {
+				["<C-h>"] = false,
+				["M-h"] = "actions.select_split",
+			},
+			view_options = {
+				show_hidden = true,
+			},
+		},
+		config = true,
 	},
 	{
 		"akinsho/bufferline.nvim",
