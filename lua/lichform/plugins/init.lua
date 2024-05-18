64ac1a422be3afe5aa899323bc300d4b6468f87f
Alessandro Gangemi <agangemi0x2a@gmail.com>
2024-05-18 16:31:00 -0400

    style: Stylua

--- lua/lichform/plugins/init.lua
+++ lua/lichform/plugins/init.lua
@@ -19,28 +19,28 @@ return {
 			require("nvim-tree").setup()
 		end,
 	},
-    {
-        "stevearc/oil.nvim",
-        dependencies = { "nvim-tree/nvim-web-devicons" },
-        config = function ()
-            require("oil").setup {
-                columns = { "icon" },
-                keymaps = {
-                    ["<C-h>"] = false,
-                    ["M-h"] = "actions.select_split",
-                },
-                view_options = {
-                    show_hidden = true,
-                },
-            }
+	{
+		"stevearc/oil.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("oil").setup({
+				columns = { "icon" },
+				keymaps = {
+					["<C-h>"] = false,
+					["M-h"] = "actions.select_split",
+				},
+				view_options = {
+					show_hidden = true,
+				},
+			})
 
-            -- Open parent directory in current window
-            vim.keymap.set("n", "-", "<cmd>Oil<cr>", { desc = "Open parent directory" })
+			-- Open parent directory in current window
+			vim.keymap.set("n", "-", "<cmd>Oil<cr>", { desc = "Open parent directory" })
 
-            -- Open parent directory in floating window
-            vim.keymap.set("n", "<space>-", require("oil").toggle_float)
-        end,
-    },
+			-- Open parent directory in floating window
+			vim.keymap.set("n", "<space>-", require("oil").toggle_float)
+		end,
+	},
 	{
 		"akinsho/bufferline.nvim",
 		dependencies = { "nvim-tree/nvim-web-devicons" },
