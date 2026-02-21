314b582c376afc0ff9cd2a12df4744cfdc51d5fb
Alessandro Gangemi <agangemi0x2a@gmail.com>
2026-02-20 21:05:35 -0500

    Add some more Neovide options

--- lua/lichform/set.lua
+++ lua/lichform/set.lua
@@ -47,7 +47,12 @@ vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 
 if vim.g.neovide then
-  vim.o.guifont = "FiraCode NFM"
+  vim.o.guifont = "FiraCode NFM:h12"
+  vim.g.neovide_opacity = 0.95
+  vim.g.neovide_title_background_color = string.format(
+    "%x",
+    vim.api.nvim_get_hl(0, {id=vim.api.nvim_get_hl_id_by_name("Normal")}).bg
+  )
 end
 
 local config_dir = vim.fn.stdpath("config")
